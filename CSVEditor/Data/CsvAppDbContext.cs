edb89058b4cff9d6b9b767ab9b4c68271e55935e
Babay <babayvolohatyu@gmail.com>
2025-09-20 14:22:27 +0300

    Add db context

--- CSVEditor/Data/CsvAppDbContext.cs
+++ CSVEditor/Data/CsvAppDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CSVEditor.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CSVEditor.Data
 {
     public class CsvAppDbContext : DbContext
     {
         public CsvAppDbContext(DbContextOptions<CsvAppDbContext> options) : base(options) { }
+
+        public DbSet<Entity> Entities { get; set; }
     }
 }
