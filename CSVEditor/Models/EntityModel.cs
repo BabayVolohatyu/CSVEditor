b24b3380401f96f4e983586f034fedb97f75364c
Babay <babayvolohatyu@gmail.com>
2025-09-20 14:00:47 +0300

    Add Entity model

---
+++ CSVEditor/Models/EntityModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CSVEditor.Models
+{
+    public class EntityModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTimeOffset DateOfBirth { get; set; }
+        public bool Married { get; set; }
+        public string Phone { get; set; } = string.Empty;
+        public float Salary { get; set; }
+    }
+}
